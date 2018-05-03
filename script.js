e8e6df7c7df10cc0954f8cc10be01f640dac3e19
Maestoso <37811799+maestosobot@users.noreply.github.com>
2018-05-03 18:36:05 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-
-  jQuery(".menu-trigger").click(function() {
-    jQuery(".navbar").slideToggle(400, function() {
-      jQuery(this).toggleClass("navbar-e").css('display', '');
-    });
-  });
-
-});
